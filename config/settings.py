a54f134747f0b4a538615f48a5216ceffc0143e8
Ryu Tanabe <bellx2@gmail.com>
2024-07-12 13:21:31 +0900

    DEBUGの環境変数化

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 from dotenv import (
     find_dotenv,
@@ -29,10 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--0x)i2t1xd114l)4ko*dmm0b-tq7@r^(+hrx80&v_ig(y-j8uh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', '').lower() == 'true'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
